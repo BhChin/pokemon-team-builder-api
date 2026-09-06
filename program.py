1a27981183433c3711515df253e8fedfa0f4e819
BhChin <brandonhchin@gmail.com>
2026-09-05 20:55:25 -0700

    print stats and searching by name

--- program.py
+++ program.py
@@ -1,4 +1,10 @@
 import sys
+import requests
+import math
+
+from team import PokemonTeam
+from requests.exceptions import HTTPError, ConnectionError, Timeout, JSONDecodeError
+
 
 # import requests
 #
@@ -29,26 +35,77 @@ def run_program():
         option = input("Select an option: ")
 
     while True:
-        if option == ('1'):
-            print("hello")
-        elif option == ('2'):
+        if option == '1':
+            pokemon = input("Enter a Pokemon Name: ")
+            data = search_by_name(pokemon)
+
+            if data is not None:
+                print_stats(data)
+
+        elif option == '2':
             pass
-        elif option == ('3'):
+        elif option == '3':
             pass
-        elif option == ('4'):
+        elif option == '4':
             pass
-        elif option == ('5'):
+        elif option == '5':
             pass
-        elif option == ('6'):
+        elif option == '6':
             sys.exit(0)
+        print_options()
+
+def run_option_1():
+    pass
+
+def search_by_name(pokemon_name: str) -> dict | None:
+    pokemon_name = pokemon_name.strip().lower()
+    pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+
+    try:
+
+        response = requests.get(pokemon_url, timeout=10) # good practice to set out a timeout
+
+        if response.status_code == 404:
+            print(f"Pokemon {pokemon_name} not found")
+            return None
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        return data
+
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error: ", errt)
+        return None
+
+    except requests.exceptions.HTTPError as err:
+        print(f"API request failed: {err}")
+        return None
 
 
 
+def print_stats(data: dict) -> None:
+    name = data["name"]
+    id = data["id"]
+    #types = data["types"]['slot']
+    height = round(data["height"]/3.048, 3) # decimeter -> feet
+    weight = round(data["weight"]/4.536, 3) # hectogram -> pound
 
-def print_title() -> None:
-    print("================================",
-          "      POKÉMON TEAM BUILDER      ",
-          "================================", sep='\n')
+    print(f"Name: {name}")
+    print(f"Pokedex ID: {id}")
+    print(f"Type: ")
+    print(f"Height: {height} ft")
+    print(f"Weight: {weight} lbs")
+
+
+def add_to_team(data: dict, team: PokemonTeam) -> None:
+    pass
+
+def search_by_weight():
+    pass
+
+
 
 def print_options() -> None:
     print("1. Search for a Pokémon",
